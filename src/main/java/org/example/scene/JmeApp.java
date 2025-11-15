cc1101f144dcafea5e0849f21e2f0af058efb046
AyoubGourstane04 <ayoubgourstan@gmail.com>
2025-11-15 20:09:48 +0100

    latest commit

--- src/main/java/org/example/scene/JmeApp.java
+++ src/main/java/org/example/scene/JmeApp.java
@@ -2,6 +2,8 @@ package org.example.scene;
 
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
@@ -35,6 +37,8 @@ public class JmeApp extends SimpleApplication {
     private SceneManager sceneManager;
     private float walkSpeed = 10f;
     private boolean moveForward, moveBackward, moveLeft, moveRight;
+    private BitmapText crosshair;
+
 
     public static void main() {
         JmeApp app = new JmeApp();
@@ -73,6 +77,7 @@ public class JmeApp extends SimpleApplication {
         // Initialiser le gestionnaire de scène
         sceneManager = new SceneManager(this);
         sceneManager.initializeScene();
+        sceneManager.initializeClickDetection();
 
         // Configurer la caméra
         setupCamera();
@@ -80,10 +85,40 @@ public class JmeApp extends SimpleApplication {
         // Configurer les contrôles
         setupControls();
 
+        // ✅ NOUVEAU : Créer le réticule
+        createCrosshair();
+
         // Cacher le curseur pour une expérience immersive
         inputManager.setCursorVisible(false);
 
     }
+
+    /**
+     * ✅ Crée un réticule au centre de l'écran
+     */
+    private void createCrosshair() {
+        BitmapFont font = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        crosshair = new BitmapText(font);
+        crosshair.setSize(font.getCharSet().getRenderedSize() * 2);
+        crosshair.setText("+");
+        crosshair.setColor(ColorRGBA.White);
+
+        // Positionner au centre de l'écran
+        float x = settings.getWidth() / 2f - crosshair.getLineWidth() / 2f;
+        float y = settings.getHeight() / 2f + crosshair.getLineHeight() / 2f;
+        crosshair.setLocalTranslation(x, y, 0);
+
+        guiNode.attachChild(crosshair);
+
+        // ✅ Message d'instructions
+        BitmapText instructions = new BitmapText(font);
+        instructions.setSize(font.getCharSet().getRenderedSize());
+        instructions.setText("Visez un tableau avec le réticule (+) et cliquez (clic gauche)");
+        instructions.setColor(ColorRGBA.Yellow);
+        instructions.setLocalTranslation(10, settings.getHeight() - 30, 0);
+        guiNode.attachChild(instructions);
+    }
+
 
     /**
      * Configure la position et les paramètres de la caméra
@@ -113,6 +148,7 @@ public class JmeApp extends SimpleApplication {
         inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
         inputManager.addMapping("Crouch", new KeyTrigger(KeyInput.KEY_LSHIFT));
         inputManager.addMapping("Exit", new KeyTrigger(KeyInput.KEY_ESCAPE));
+        inputManager.addMapping("Click", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
 
         // Listener pour les actions
         ActionListener actionListener = new ActionListener() {
@@ -150,31 +186,83 @@ public class JmeApp extends SimpleApplication {
                             stop();
                         }
                         break;
+                    case "Click":
+                        if (isPressed) {
+                            sceneManager.detectPaintingClick();
+                        }
+                        break;
                 }
             }
         };
 
         inputManager.addListener(actionListener, "Forward", "Backward", "Left", "Right",
-                "Jump", "Crouch", "Exit");
+                "Jump", "Crouch", "Exit", "Click");
     }
+
+
+    private Vector3f lastCamPos = new Vector3f();
+    private boolean isMoving = false;
 
     @Override
     public void simpleUpdate(float tpf) {
-        // Vous pouvez ajouter ici la logique de mise à jour personnalisée
-        // Par exemple, limiter la hauteur de la caméra pour simuler la marche
         Vector3f camPos = cam.getLocation();
-        if (camPos.y < 2f) {
-            cam.setLocation(new Vector3f(camPos.x, 2f, camPos.z));
-        }
-        if (camPos.y > 8f) {
-            cam.setLocation(new Vector3f(camPos.x, 8f, camPos.z));
+
+        if (camPos.distance(lastCamPos) > 0.05f) {
+            if (!isMoving) {
+                sceneManager.playAnimation("Walk");
+                isMoving = true;
+            }
+        } else {
+            if (isMoving) {
+                sceneManager.playAnimation("Idle");
+                isMoving = false;
+            }
         }
 
-        if(sceneManager.getRobot() != null){
-            Vector3f offsetPosition = new Vector3f(2f,-3f,-2f);
-            sceneManager.getRobot().setLocalTranslation(camPos.add(offsetPosition));
+        lastCamPos.set(camPos.clone());
+
+        if (camPos.y < 2f) cam.setLocation(new Vector3f(camPos.x, 2f, camPos.z));
+        if (camPos.y > 8f) cam.setLocation(new Vector3f(camPos.x, 8f, camPos.z));
+
+        // ✅ Robot DEVANT la caméra
+        if(sceneManager.getRobot() != null) {
+            Vector3f camDirection = cam.getDirection().normalize();
+            Vector3f robotPos = sceneManager.getRobot().getLocalTranslation();
+
+            float distanceInFront = 3.5f;
+
+            Vector3f offset = new Vector3f(
+                    camDirection.x * distanceInFront,
+                    0,
+                    camDirection.z * distanceInFront
+            );
+
+            Vector3f newPos = new Vector3f(
+                    camPos.x + offset.x,
+                    robotPos.y,
+                    camPos.z + offset.z
+            );
+
+            sceneManager.getRobot().setLocalTranslation(newPos);
+            sceneManager.getRobot().lookAt(camPos, Vector3f.UNIT_Y);
         }
 
+        // ✅ NOUVEAU : Mettre à jour la bulle d'info et le mouvement du robot
+        sceneManager.update(tpf, cam);
+
+        // ✅ NOUVEAU : Changer couleur du réticule
+        updateCrosshairColor();
+    }
+
+    /**
+     * ✅ Change la couleur du réticule selon ce qu'on vise
+     */
+    private void updateCrosshairColor() {
+        if (sceneManager.isLookingAtPainting()) {
+            crosshair.setColor(ColorRGBA.Red);
+        } else {
+            crosshair.setColor(ColorRGBA.White);
+        }
     }
 
 
