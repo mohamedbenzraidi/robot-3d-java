61f232b5064396a77db7475246023bc65f3d35ea
mohamedbenzraidi <mohamedbenzraidi@gmail.com>
2025-12-03 00:35:26 +0100

    troisieme musée

--- src/main/java/org/example/robot/RobotManager.java
+++ src/main/java/org/example/robot/RobotManager.java
@@ -1,16 +1,9 @@
 package org.example.robot;
 
-import com.jme3.app.SimpleApplication;
-import com.jme3.asset.AssetKey;
 import com.jme3.asset.AssetManager;
-import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
 import com.jme3.texture.Texture;
 import org.example.scene.AssetLoader;
 
